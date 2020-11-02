548e86a49bd71584cd3341080d7ecb933bd523e7
liesse97 <ishimwelieish@gmail.com>
2020-11-02 12:29:06 +0100

    branch change

--- cart.js
+++ cart.js
@@ -1,9 +1,35 @@
-let remover = document.querySelector(".fa-trash-alt");
-// let tak= document.querySelector(".take-away")
+const remover = document.querySelectorAll(".fa-trash-alt");
 
-// for(var i=0 ; i < remover.length ; i++){
-// var removebtn = remover[i]
-remover.addEventListener('click',(event)=> {
-    console.log(remover)
+
+for(var i=0 ; i < remover.length ; i++){
+     let removebtn = remover[i]
+removebtn.addEventListener('click',(event)=> {
     event.target.parentNode.parentNode.remove();
+    updateSum()
 })
+}
+
+function updateSum(){
+const cartcontain= document.getElementsByClassName('rowtotal')[0];
+const cartRow= cartcontain.getElementsByClassName('row');
+let total=1;
+
+for(var i= 0;i < cartRow.length;i++){
+let cartRows=cartRow[i]
+let prices=cartRows.getElementsByClassName('price')[0];
+let quantity=cartRows.getElementsByClassName('cartQuant')[0];
+let kronor= prices.innerText;
+let quanti= quantity.value
+total= total + (kronor * quanti);
+}
+document.querySelectorAll('.totalprice')[0].innerText= total;
+}
+
+//
+
+let quantity=cartRows.getElementsByClassName('cartQuant');
+for(var i=0; i<quantity.length;i++){
+    let input=quantity[i]
+    input.addeventListener('change,quatintychange')
+
+}
--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const menu2 = document.querySelector(".hamb-menu");
 const navbar= document.querySelector(".menu");
 
-const green= document.querySelector(".svg2");
 
-const merInfo= document.querySelector('.fa-angle-down');
-const minInfo= document.querySelector('.fa-angle-up');
 
 
 
@@ -17,32 +14,6 @@ menu2.addEventListener("click",() => {
 
 });
 
-if (green) {
-    green.addEventListener("click",() => {
-    // setTimeout(()=> {
-    //    document.querySelector('.hid').style.display = 'block';}, 500)   
-    document.querySelector('.hid').style.display = 'block';
-    });
-}
-if (merInfo) {
-merInfo.addEventListener("click",()=>{
-    document.querySelector('.hide2').style.display='block';
-    document.querySelector('.hide').style.display='none';
-});
-}
-if (minInfo) {
-minInfo.addEventListener("click",()=>{
-    document.querySelector('.hide').style.display='block';
-    document.querySelector('.hide2').style.display='none';
 
-});
-}
-// console.log(remover)
-// for(var i=0 ; i < remover.length ; i++){
-// var removebtn = remover[i]
 
-// remover.addEventListener("click",()=>{
-//     console.log('event');
-//     event.target.parentNode.remove()
-// });
 
